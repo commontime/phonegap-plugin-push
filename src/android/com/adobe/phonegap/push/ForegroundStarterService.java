1ce79abe9d4cdab2d4974c60d27cf5bb8b711e27
Graham Mackenzie <gmackenzie@commontime.com>
2019-05-15 14:56:40 +0100

    Add files via upload

---
+++ src/android/com/adobe/phonegap/push/ForegroundStarterService.java
@@ -0,0 +1,58 @@
+package com.commontime.cmpush;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import com.cti.cm.dev.R;
+
+public class ForegroundStarterService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        if (intent.getAction().equals("start")) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel("1", "1", NotificationManager.IMPORTANCE_HIGH);
+                channel.setDescription("Channel 1");
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.createNotificationChannel(channel);
+            }
+
+            Notification notification = new NotificationCompat.Builder(this, "1")
+                    .setContentTitle("Truiton Music Player")
+                    .setTicker("Truiton Music Player")
+                    .setContentText("My Music")
+                    .setSmallIcon(R.drawable.common_google_signin_btn_icon_light)
+                    .setOngoing(true).build();
+
+            startForeground(0, notification);
+        }
+
+        if (intent.getAction().equals("stop")) {
+            stopSelf(startId);
+            return START_NOT_STICKY;
+        }
+
+        return START_STICKY;
+    }
+}
